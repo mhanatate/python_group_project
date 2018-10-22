ab890ae7fc6ee9f49e2fd3cb0fafe08e64663924
mhanatate <43131890+mhanatate@users.noreply.github.com>
2018-10-22 15:24:24 -0700

    yoooo (#8)
    * middleman
    
    * removed test code
    
    * all
    
    * Revert "middleman"
    
    This reverts commit 59980cf686beaa4eddc2b480b30b4b763c9883ce.
    
    * ready to test reeeel quick
    
    * pycache

--- group_project/apps/project_app/views.py
+++ group_project/apps/project_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+import requests
 from django.contrib import messages
 from apps.project_app.models import User
 import bcrypt
+
 
 def index(request):
     if 'id' not in request.session or request.session['id'] == None:
@@ -49,22 +51,55 @@ def validate_login(request):
 def wheel(request):
     return render(request, "project_app/wheel.html")
 
+def process_wheel(request):
+    
+    return redirect("/test")
+
 def preferences(request):
     return render(request, "project_app/preferences.html")
 
 def process_preferences(request):
+    request.session['type'] = request.POST['type']
+    request.session['price'] = request.POST['price']
+    request.session['rating'] = request.POST['rating']
     return redirect('/wheel')
+
+def results(request):
+    return render(request, "project_app/test_subject.html")
 
 def success(request):
     data = User.objects.get(id=request.session['id'])
     userdict = {
         "datakey": data
-        
     }
-    print('helllooooooooooooooooo')
     return render(request, "project_app/success.html", userdict)
 
 def logout(request):
     request.session['id'] = None
     return redirect('/')
 
+def yelpAPI(request):
+    is_cached = ('business' in request.session)
+
+    if not is_cached:
+        zip_code = 98006
+        response = requests.get('https://api.yelp.com/v3/businesses/search/%s' % zip_code)
+        request.session['business'] = response.json()
+
+    business = request.session['business']
+
+    return render(request, 'project_app/testsubject.html', {
+        'mileradius' : business['radius'],
+        'location' : business['location'],
+        'latitude': business['latitude'],
+        'longitude': business['longitude'],
+        'phone' : business['phone'],
+        'url' : business['url'],
+        'rating' : business[ 'rating'],
+        'review_count' : business[ 'review_count'],
+        'price' : business['price'],
+        'name': business['name'],
+        'categories': business['categories'],
+        'is_cached': is_cached,
+        'api_key': '8fJisUcWi6_6M8q1TqXwV64duaoO7p6rs5Sh4xI9b6abzOxLgAHFW_OrD2jgX7rRH0a2bwm4Uhio4-5JiVQCbTHyvrzs8667unV_strpWIR6xq-CLwuT5V-uBH3KW3Yx',  # Don't do this! This is just an example. Secure your keys properly.\
+    })
